e1f37ddb573ca754742828703e6a6f2e59a280f8
Alireza Ch <94705144+TheGray613@users.noreply.github.com>
2022-09-03 11:04:56 +0430

    And or not
    3 example for understanding and, or, not

---
+++ andornot.py
@@ -0,0 +1,9 @@
+#aya 15 bozorg tar az 12 ast va 15 bozorgtar az 13.8 
+print (15>12 and 15>13.8)
+
+a=300
+print(a)
+print (not a == 300)
+
+print("hello" == "hi")
+print("hello" == "hi" or a == 300)
