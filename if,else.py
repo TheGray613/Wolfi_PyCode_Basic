fb2e43e80f8ffdb6816f1910dbf79090f8cda77f
Alireza Ch <94705144+TheGray613@users.noreply.github.com>
2022-09-03 11:56:53 +0430

    If and elif and else
    3 example for if and else and a code with convert and condition(shart)

---
+++ if,else.py
@@ -0,0 +1,26 @@
+#if Condition:
+    #do something
+#elif Condition:(bishtar az chand bar mishe azash estefade kard)
+    #do something
+#else:
+    #do something else
+
+if 3>2:
+    print("Three bigger than Two")
+
+a = 4
+if a < 2:
+    print("hello")
+else:
+    print("value incorecct")
+
+
+my_score = int(input("pls enter youre score: "))
+if my_score == 20 :
+    print("aswesome(afalin khafan)")
+elif my_score >= 18 :
+    print ("very good(afalin pesar\dokhtar)")
+elif my_score >= 15 :
+    print ("good(afalin)")
+else :
+    print ("not bad(afalin,vali bishtar dars bekhun)")
