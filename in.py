34f6c10ec64eda90f7ce8f11dbf15b2432a7441f
Alireza Ch <94705144+TheGray613@users.noreply.github.com>
2022-09-03 16:04:31 +0430

    In functtion
    if you wanna learn how we used in function, check these examples !

---
+++ in.py
@@ -0,0 +1,12 @@
+#aya a dar hurufi ke dadim hast ya na  ?
+print ("a" in "ayrtsjkhhffjha")
+#aya 2 dar liste 1, 2, 3, 4, 5, 6 hast ya na ?
+print (2 in [1, 2, 3, 4, 5])
+
+str1 = input ("enter a word: ")
+str2 = " this is a beautiful bike"
+
+if str1 in str2:
+    print ("yes")
+else:
+    print ("no")
