854fd24c02cdb1d66bd9da1b68285e6122cdf871
Alireza Ch <94705144+TheGray613@users.noreply.github.com>
2022-09-08 19:56:05 +0430

    For function
    founder, will be search in you're string and give you search result

---
+++ ForWIthExampleFounder.py
@@ -0,0 +1,6 @@
+My_string = ("Ready for war")
+for Founder in My_string:
+     if Founder == "W":
+         print("hey i found W in you're string,say me good job")
+     else :
+         print ("Sorry mate, i can not find W in you're string,here you're cash.")
