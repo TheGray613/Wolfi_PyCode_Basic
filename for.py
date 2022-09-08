636982d00a468bec134e076d0dea14593846cc96
Alireza Ch <94705144+TheGray613@users.noreply.github.com>
2022-09-08 19:45:01 +0430

    For
    Ready to learning for

---
+++ for.py
@@ -0,0 +1,20 @@
+
+for num in [1, 2, 3, 4, 5]:
+    print(num)
+print("--------------------------------")
+for num in [6, 7, 8, 9]:
+    print(num*2)
+
+names = ["parisa", "setare", "fateme"]
+for shokolat in names:
+    print (shokolat)
+
+colors = ["blue", "black", "white"]
+for color in colors:
+    print (color * 25)
+
+#video 3
+
+my_str = " wolfi is the best"
+for new in my_str:
+    print (new, 1)
