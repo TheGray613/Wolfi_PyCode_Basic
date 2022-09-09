e22760d8a300dbbbf4fb5920a9594aada2794099
Alireza Ch <94705144+TheGray613@users.noreply.github.com>
2022-09-09 20:41:06 +0430

    Break with while, if else
    Break command with while, if, else

---
+++ Break.py
@@ -0,0 +1,20 @@
+for Numbers in [90, 91, 92, 93, 94, 95]:
+    if Numbers == 93:
+        break
+    print(Numbers)
+
+print ("The End")
+
+num = 0
+while num <100:
+    if num == 5:
+        break
+    print(num)
+    num = num + 1
+print ("End")
+
+my_string = input("enter here:")
+while my_string != "exit":
+    print(my_string)
+    my_string = input("enter another one: ")
+print ("end")
