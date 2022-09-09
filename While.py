18f329efe9ac1c8e401bb4de51964208083822fd
Alireza Ch <94705144+TheGray613@users.noreply.github.com>
2022-09-09 10:38:46 +0430

    while function
    you will learning while function with two easy example

---
+++ While.py
@@ -0,0 +1,28 @@
+#while syntax
+#while Condition:
+    #do something
+    #...
+    #...
+#else:
+    #do something
+    #...
+
+#halqe bi nahayat
+from tkinter import N
+
+
+number = 10
+while number < 15:
+    print(number)
+    number = number + 1
+else:
+    print("operation finished")
+#dasture print badi kharej az badane while hastesh.
+print("this operation is end")
+
+
+my_string = "lets go"
+while my_string == "lets go":
+    print("yeah, our string is same !")
+    my_string = "not right now"
+print ("every thing is ok and opration is finished")
