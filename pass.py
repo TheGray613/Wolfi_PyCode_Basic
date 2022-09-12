f9ed5a90c8fc601721f8dc1b1d6ab8cb82160d02
Alireza Ch <94705144+TheGray613@users.noreply.github.com>
2022-09-12 15:38:51 +0430

    Pass
    Simple code foe pass the line

---
+++ pass.py
@@ -0,0 +1,7 @@
+#pass komak mikone be ma ke un shart ro rad konim,be nazar miad ke be darde bypass kardane jomlei ke ma rush error darim ham mikhore
+print ("oggy is fucked up")
+
+for number in [1, 2, 3, 4, 5]:
+    pass
+print ("beetle(susk ha) is winner")
+
