5ae7463c805ae54e4f2c0ed9cafbc4dcba7c1a6e
Alireza Ch <94705144+TheGray613@users.noreply.github.com>
2022-09-17 14:47:11 +0430

    Learning range with Underline
    The point about Under Line !

---
+++ Range.py
@@ -0,0 +1,10 @@
+for num in [1, 2, 3, 4]:
+    print("hello")
+
+#ma az under line estefade kardim ta motevajeh beshim serfan az _ nemikhaym estefade konim baraye vurudi,yani aslan baraye ma mohem nistesh
+for _ in range(0, 23):
+    print("Wolfi is the best")
+
+#hala behesh gam shomar ham dadim, yani ba qadam haye 2 taei pish boro !
+for _ in range (1, 13, 2):
+    print (_)
