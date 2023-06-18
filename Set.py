20634a629118cfeee015cb1f63261b71bb24bf73
Alireza Wolfi613 <94705144+TheGray613@users.noreply.github.com>
2023-06-18 11:38:01 +0330

    Set
    With 2 example and delete same value in a list

--- Set.py
+++ Set.py
@@ -1,2 +1,11 @@
-#sets
-mt_set = set([1, 1, 'nooranet'])
+#set attribute is really rare, meaning of set: when you make two value which equal auanity(meqdar) i(set) show you just one of that.
+Wolfi = set()
+Wolfi.add(4)
+Wolfi.add(4)
+Wolfi.add(7)
+print(Wolfi)
+
+My_Lovele_List = ["atom", "atom", 23, 23, "red", "blue"]
+print(list(set(My_Lovele_List)))
+
+# can we delete same value in a list with using !? 1_Yes 2_No 3_go to hell 4_none 
