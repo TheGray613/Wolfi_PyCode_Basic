8c5483bdd1affde923de203de9d63a153ba086cb
Alireza Wolfi613 <94705144+TheGray613@users.noreply.github.com>
2023-06-18 11:10:59 +0330

    Important reserved key words
    do you know how many words can not be used in our program ? lets check it.

---
+++ Reserced_Keywords.py
@@ -0,0 +1,8 @@
+#we wanna check all important reserved key words.
+#you can not used this words directly
+Reserved_Words = ("AND", "EXEC", "NOT", "ELIF", "ASSERT", "FINALLY", "OR", "IN", "BREAK", "FOR", "PASS", "WHILE", "CLASS", "FROM", 
+                  "PRINT", "ELSE", "CONTINUE", "GLOBAL", "RAISE", "IS", "DEF", "IF", "RETURN", "WITH", "IMPORT", "TRY", "EXCEPT", "DEL", "LAMBDA", 
+                  "YEILD", "FALSE" ,"TRUE")
+print (Reserved_Words)
+#Can you tell me what is Reserced_Words type ? 1_list 2_dict 3_tupple 4_none
+# can you tell me what is most attribute about last question ? 
