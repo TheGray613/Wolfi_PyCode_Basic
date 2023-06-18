8625c40d060ba502cf6f9c8cf3dc99978a3d7b22
Alireza Wolfi613 <94705144+TheGray613@users.noreply.github.com>
2023-06-18 17:19:33 +0330

    Data type start of python
    how many data type available in python ? lets learn it.

---
+++ DataType.py
@@ -0,0 +1,17 @@
+str = "Wolfi Is Kind With People"		
+int= 23		
+float = 20.5		
+complex = 1j		
+list = ["Wolf", "ChildOfWolf", "ChildOfChildWolf"]		
+tuple = ("Geri", "Freki", "Odin")		
+range = range(6)		
+dict = {"OdinWolfName" : "geri", "age" : 999}		
+set = {"food", "meat", "human"}		
+frozenset = frozenset({"aggressive", "calm", "cold"})		
+bool = True		
+bytes = b"animal"		
+bytearray = bytearray(5)		
+memoryview = memoryview(bytes(5))		
+NoneType = None	
+
+#can you tell me how many type available in python for making code(just try to count our code) ? A_1 B_4 C_15 D_none
