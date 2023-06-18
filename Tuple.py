a233cd0cdbc373f21918734c3f53f98ae3cc3bcb
Alireza Wolfi613 <94705144+TheGray613@users.noreply.github.com>
2023-06-18 11:13:30 +0330

    Tuples
    Lets learn how to use tuple(with 2 example)

---
+++ Tuple.py
@@ -0,0 +1,10 @@
+#Tuples look likes list but used () 
+#you can not change values in tuple
+
+My_Tuple = (1, 2.0, 'Wolfi')
+print (My_Tuple)
+print(len(My_Tuple))
+print(My_Tuple[3])
+
+#which difference between list and tupple ? 
+#1_nothing 2_value in tupple will be change 3_list have a most used in prograaming 4_value in tupple can not be change 
